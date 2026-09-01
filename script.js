46ba467563dc5078235bab8a0ce81624d9f1164b
Jaqq0 <vehmas.timo@gmail.com>
2026-09-01 22:19:22 +0300

    pelikentän sekä pelaajien mailojen piirto

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
+// kenttä
 let board;
 let boardWidth = 500;
 let boardHeight = 500;
 let context;
+
+//pelaajat
+let playerWidth = 10;
+let playerHeight = 50;
+
+let player1 = {
+    x: 10,
+    y: boardHeight / 2,
+    width: playerWidth,
+    height: playerHeight,
+}
+
+let player2 = {
+    x: boardWidth - playerWidth - 10,
+    y: boardHeight / 2,
+    width: playerWidth,
+    height: playerHeight,
+}
 
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d");
+
+    requestAnimationFrame(update);
+}
+
+function update() {
+    requestAnimationFrame(update);
+
+    context.fillStyle = "skyblue";
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+    context.fillRect(player2.x, player2.y, player2.width, player2.height);
 }
